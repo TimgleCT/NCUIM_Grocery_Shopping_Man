654d2f798d76c959700554cbe9aea7d9fd541ac7
jeffrey <jeffreyyeh722@gmail.com>
2019-12-05 16:18:55 +0800

    增加Product app

--- NCU_Grocery_Shopping_Man/urls.py
+++ NCU_Grocery_Shopping_Man/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from members.views import Index,Login,Registered,Product
 
 urlpatterns = [
@@ -22,5 +22,5 @@ urlpatterns = [
     path('index/', Index),
     path('login/', Login),
     path('registered/', Registered),
-    path('product/', Product),
+    path('products/', include('products.urls')),
 ]
