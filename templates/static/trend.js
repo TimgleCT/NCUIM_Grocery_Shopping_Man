e3c0b253d9b83f79215800d393ec90725a379779
Alex <alex112401@gmail.com>
2019-12-18 22:12:29 +0800

    趨勢圖發大財

--- templates/static/trend.js
+++ templates/static/trend.js
@@ -1,38 +1,61 @@
 
 function gd(year, month, day) {
-    return new Date(year, month-1, day+17).getTime()-1440000000;
+    // return new Date(year, month-1, day+17).getTime()-1440000000;
+    return new Date(year, month-1, day).getTime()+29160000;
 }
 
 function dateconvert(Data_prod) {
-    strig_d = Data_prod[0].toString();
-    test = strig_d.split(".");
-    year = strig_d.split(".")[0];
-    month = strig_d.split(".")[1];
-    day = strig_d.split(".")[2];
-    return test;
+
+    for(i=0;i<7;i++){
+        if(Data_prod[i]!=null){
+            string_d = Data_prod[i].Date;
+            string_p = Data_prod[i].AveragePrice;
+            o_test = string_d.split(".");
+            o_year = string_d.split(".")[0];
+            o_month = string_d.split(".")[1];
+            o_day = string_d.split(".")[2];
+            console.log(o_test);
+            console.log(o_year);
+            console.log(o_month);
+            console.log(o_day);
+            console.log(gd(parseInt(o_year),parseInt(o_month),parseInt(o_day)),string_p);
+            // console.log(gd(108,12,17));
+            week_data[i] = [gd(parseInt(o_year),parseInt(o_month),parseInt(o_day)),string_p];
+            console.log(week_data[i]);
+        }
+    }
+
 }
+
+var week_data = [];
+
+
 
 var testdata = [
     [gd(2019,12,17),94.87],
     [gd(2019,12,18),88],
     [gd(2019,12,19),87.87],
     [gd(2019,12,20),77.77],
-    [gd(2019,12,21),99.77],
-    [gd(2019,12,22),88.77],
-    [gd(2019,12,23),99]
-]
+    [gd(2019,12,21),99.77]
+    // [gd(2019,12,22),88.77],
+    // [gd(2019,12,23),99]
+];
 
 
 
-var trendDate, trendMarketName, trendProductName, trendPrice
-
+$(document).ready(function () {
+    dateconvert(trend_data);
+    // console.log('function work');
+});
 
 $(function () {
+    //
+    // console.log('function1 work');
 
     $.plot($("#flotcontainer"),
         [
             {
-              data: testdata,
+              data: week_data,
               points: { show: true },
               lines: { show: true}
             }
